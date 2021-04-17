1a51bb27aebd798e7a7aec03d602d009dc30d025
panxingyu.icer <panxingyu.icer@bytedance.com>
2021-04-17 19:12:55 +0800

    Initial commit

---
+++ wechat_service/services/test.py
@@ -0,0 +1,9 @@
+import json
+from django.http import HttpResponse, Http404, HttpResponseBadRequest
+
+from wechat_service.utils.request_processor import fetch_parameter_dict
+
+
+def test_api(request):
+    parameter_dict = fetch_parameter_dict(request, 'POST')
+    return HttpResponse(json.dumps({'message': parameter_dict['username'] + ' welcome!'}))
